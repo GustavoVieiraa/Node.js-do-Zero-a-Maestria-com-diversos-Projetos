8b1a26e018db1fbdab66f20daf559da81983e771
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-16 20:17:55 -0300

    Event Loop - concluído com êxito.
    Event Loop - concluído com êxito.

---
+++ Fundamentos/Event Loop/index.js
@@ -0,0 +1,15 @@
+function a() {
+    console.log("Executando a()");
+}
+
+function b() {
+    console.log("Executando b()");
+}
+
+function c() {
+    console.log("Executando c()");
+    a()
+    b()
+}
+
+c()
