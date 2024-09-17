8febdbc480a06bf5a08306f9edf377caedbce7aa
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-17 10:03:58 -0300

    Retornando HTML - concluído com êxito.
    Retornando HTML - concluído com êxito.

---
+++ Node Core Modulos - Prática/Retornando HTML/index.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Contenty-Type', 'text/html');
+    res.end('<h1>Olá mundo!</h1>');
+});
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`);
+});
