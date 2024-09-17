c275464777fe470292c74523d9a616a7e3489884
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-17 10:37:05 -0300

    FS - concluído com êxito.
    FS - concluído com êxito.

---
+++ Node Core Modulos - Prática/FS/index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require('fs');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+
+    fs.readFile('mensagem.html', function(err, data) {
+
+        res.writeHead(200, { 'Contenty-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+
+    });
+
+
+});
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`);
+});
