c4f71bb9a145490d2c1d4fb233c8f0eefa4fb0f1
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-23 18:22:54 -0300

    CSS com Express - concluído com êxito.
    CSS com Express - concluído com êxito.

---
+++ Express/CSS com Express/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+const basePath = path.join(__dirname, 'templates')
+
+const users = require('./users')
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use('/users', users)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`|App ON| Running PORT ${port} ...`)
+})
+
