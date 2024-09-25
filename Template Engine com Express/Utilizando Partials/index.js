793b62d0acbfd2f0b650f639bfc1cf09f0b5b369
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-25 08:38:19 -0300

    Utilizando Partials - concluído com êxito.
    Utilizando Partials - concluído com êxito.

---
+++ Template Engine com Express/Utilizando Partials/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+})
+const PORT = 3000
+const app = express()
+
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+    const users = [
+        {
+            name: 'Gustavo',
+            lastname: 'Vieira',
+            age: 21,
+            createAccount: '25/09/2024',
+            isBeta: true,
+            permissoes: [
+                'VIP', 'Admin', 'Helper', 'Staff'
+            ]
+        },
+        {
+            name: 'Rafae',
+            lastname: 'Dutra',
+            age: 25,
+            createAccount: '25/09/2024',
+            isBeta: false,
+            permissoes: [
+                'VIP', 'Admin', 'Helper', 'Staff'
+            ]
+        }
+    ]
+
+    res.render('dashboard', {users})
+})
+
+
+app.get('/', (req, res) => {
+
+    const auth = true
+
+    res.render('home', {auth})
+})
+
+
+app.listen(PORT, () => {
+    console.log('App running...')
+})
