224b30fc62255114774c6d97fae371a74b68cbb6
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-09-26 08:19:31 -0300

    Inserindo dados no Banco - concluído com êxito.
    Inserindo dados no Banco - concluído com êxito.

---
+++ Node.JS com MySQL/Inserindo dados no Banco/index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+    const title = req.body.title
+    const pagesqty = req.body.pagesqty
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pagesqty}')`
+
+    conn.query(sql, function(err)  {
+        if(err) {
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+
+})
+
+const conn = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',   
+        password: '',
+        port: 8000,
+        database: 'nodemysql'
+    }
+)  
+
+conn.connect(function(err) {
+
+    if(err) {
+        console.log(err)
+    }
+
+    console.log('Conectou ao MySQL!')
+
+    app.listen(3000)
+
+})
