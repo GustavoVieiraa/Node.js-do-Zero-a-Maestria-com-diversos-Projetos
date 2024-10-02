b5dd94bfd16b370b59a1ed0b32e66d03d6cd32b5
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2024-10-02 08:40:48 -0300

    Criando o Model - concluído com êxito.
    Criando o Model - concluído com êxito.

---
+++ Sequelize com Node.JS/Criando o Model/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+
+const User = require('./models/User')
+
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+conn.sync().then(() => {
+    app.listen(3000)
+}).catch((err) => console.log(err))
